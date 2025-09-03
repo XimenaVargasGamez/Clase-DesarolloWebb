1ee5347a035fafe18e3c3610b25c92c09af294b8
Ximena Vargas <ximenavargas.work@gmail.com>
2025-09-02 20:23:40 -0700

    Added files

--- RecursividadClasee/Ejemplo1/recursiveFunctions.js
+++ RecursividadClasee/Ejemplo1/recursiveFunctions.js
@@ -6,3 +6,11 @@ function cuentaRegresivaConLoop(numero) {
 }
 
 cuentaRegresivaConLoop(10);
+
+const power = (base, exponent) => {
+    let result = 1;
+    for (let count = 0; count < exponent; count++) {
+        result *= base;
+    }
+return result;
+}
