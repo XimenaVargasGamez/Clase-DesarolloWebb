389c83f4831e5889bfe0c6a960d65a5e4aebf5b7
Ximena Vargas <ximenavargas.work@gmail.com>
2025-10-01 00:54:30 -0700

    Trabajo en Clase - LOGS

---
+++ Logs/EjemplosClase/AppBlogger/server.js
@@ -0,0 +1,27 @@
+const express = require( 'express');
+const app = express();
+const port = 3000;
+const upload = multer({ dest: 'uploads/' });
+
+app.use(express.static(__dirname));
+
+app.post('/subir.perfil', upload.single('pfp'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('No se subió archivo');
+  }
+
+  const rutaTemporal = req.file.path;
+  const rutaFinal = path.join(__dirname, 'uploads', 'perfilUser.png');
+
+  fs.rename(rutaTemporal, rutaFinal, (err) => {
+    if (err) {
+      return res.status(500).send('error al guardar archivo');
+    }
+    res.send('El archivo se subió correctamente');
+  });
+});
+
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
---
+++ Logs/EjemplosClase/ejemplo1/server.js
@@ -0,0 +1,41 @@
+const express = require( 'express');
+const app = express();
+const port = 3000;
+
+const fs = require('fs');
+app.get('/crear.log', (req, res) => {
+    const fecha = new Date().toLocalestring();
+        fs.writeFile('log.txt', `Log creado en: $(fecha}`, (error) => {
+            if (error) {
+                return res.status (500) .send("Error al escribir el archivo.");
+            }
+        res. send("Archivo de log creado con éxito.");
+    });
+});
+
+app.get ('/leer.log', (req, res) => {
+    fs.readFile('log.txt', 'utf8', (error, datos) => {
+        if (error) {
+            return res.status(500).send("Error al leer el archivo. ¿Lo creaste primero?");
+        }
+        res.type('text/plain').send(datos);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+    });
+
+const { exec } = require('child process');
+    app.get ('/info-cpu', (reg, res) => {
+        exec ('uname -a', (error, stdout, stderr) => {
+            if (error) {
+                return res.status (500). send (`Error al ejecutar comando: ${stderr}`);
+            }
+            res.type('text/plain').send(`Info del sistema: \n ${stdout}`);    
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
